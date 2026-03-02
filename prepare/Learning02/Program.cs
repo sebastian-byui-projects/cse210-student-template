662e02eec9672a3a1313ad48a1a58b1a93430a76
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2026-03-02 09:56:27 -0700

    Job Resume classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,56 @@
-using System;
+namespace Learning02;
 
-class Program
+internal abstract class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        var resume = new Resume
+        {
+            Name = "John Doe"
+        };
+        var job1 = new Job
+        {
+            Company = "Company A",
+            JobTitle = "Software Engineer",
+            StartYear = 2015,
+            EndYear = 2018
+        };
+        var job2 = new Job
+        {
+            Company = "Company B",
+            JobTitle = "Senior Software Engineer",
+            StartYear = 2018,
+            EndYear = 2021
+        };
+        resume.Jobs = [job1, job2];
+
+        Console.WriteLine($"Name: {resume.Name}");
+        Console.WriteLine("Jobs:");
+        resume.Display();
+    }
+}
+
+internal class Job
+{
+    public string Company { get; init; }
+    public string JobTitle { get; init; }
+    public int StartYear { get; init; }
+    public int EndYear { get; init; }
+
+    public void Display()
+    {
+        Console.WriteLine($"{JobTitle} ({Company}) {StartYear} - {EndYear}");
+    }
+}
+
+internal class Resume
+{
+    public string Name { get; set; }
+    public List<Job> Jobs { get; set; }
+
+
+    public void Display()
+    {
+        Jobs.ForEach(job => job.Display());
     }
 }
