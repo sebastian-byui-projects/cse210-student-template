0f8c966b2d2d7164c4ed60b250ab52b4761b4823
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2026-03-09 10:25:46 -0600

    Fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,55 @@
+namespace Learning03;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _bottom = 1;
+        _top = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using Learning03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        var fration1 = new Fraction();
+
+        for (int i = 0; i < 19; i++)
+        {
+            var random = new Random();
+            var numerator = random.Next(1, 10);
+            var denominator = random.Next(1, 10);
+            fration1.SetTop(numerator);
+            fration1.SetBottom(denominator);
+            Console.WriteLine($"Fraction 1: {fration1.GetFractionString()} Number: {fration1.GetDecimalValue()}");
+        }
     }
 }
