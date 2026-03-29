8d33d1bf7b8b0dcb25d4d2107d28317f6a50c8cc
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2026-03-29 17:50:30 -0600

    comment

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,10 +1,15 @@
 namespace Develop05;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        GoalManager manager = new GoalManager();
-        manager.Start();
-    }
+class Program 
+{ 
+    static void Main(string[] args) 
+    { 
+        /* EXCEEDING REQUIREMENTS:
+           Added a dynamic leveling system that calculates your level and assigns
+           a fun rank title like 'Level 13 Ninja Unicorn' based on your total points
+           to increase gamification.
+        */ 
+        GoalManager manager = new GoalManager(); 
+        manager.Start(); 
+    } 
 }
